8860bc7e3d0b9edbedf7e0e2b061e69891cdd640
Richard Kužela <richard@kanshen.click>
2023-12-29 11:07:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("input#mathscriptuserinput");
-const output = document.querySelector("mathcontainer>math");
+const output = document.querySelector("mathcontainer");
 
 input.addEventListener("input", (event) => {
     // This function will be called whenever the input event occurs
-    output.innerHTML = event.target.value.MSConvertToHTML(false);
+    output.innerHTML = event.target.value.MSConvertToHTML(true);
 });
